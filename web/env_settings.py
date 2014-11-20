bee28669e42d0041d04646fa68c82f9f6918d5ba
Petr Devaikin <p.devaikin@gmail.com>
2014-11-20 17:17:01 +0300

    env config int

--- web/env_settings.py
+++ web/env_settings.py
@@ -5,7 +5,7 @@ def load_env(app):
 
     if 'MAX_DIFF' in environ: app.config['MAX_DIFF'] = environ.get('MAX_DIFF')
 
-    if 'SECRET_KEY' in environ: app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
+    if 'SECRET_KEY' in environ: app.config['SECRET_KEY'] = int(environ.get('SECRET_KEY'))
 
     if 'FACEBOOK_APP_ID' in environ: app.config['FACEBOOK_APP_ID'] = environ.get('FACEBOOK_APP_ID')
     if 'FACEBOOK_APP_SECRET' in environ: app.config['FACEBOOK_APP_SECRET'] = environ.get('FACEBOOK_APP_SECRET')
